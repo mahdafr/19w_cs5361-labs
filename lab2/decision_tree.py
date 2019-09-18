64a6ea78dd432929eb7fef1dcf5da82fdb8acb42
Mahdokht Af <mmafravi@gmail.com>
2019-09-18 11:25:30 -0600

    updated to include instr-provided code

--- lab2/decision_tree.py
+++ lab2/decision_tree.py
@@ -1,10 +1,11 @@
 # Program to build a one-node decision tree
 # Programmed by Olac Fuentes
-# Last modified September 11, 2019
+# Last modified September 16, 2019
 
 import numpy as np
 import time
-        
+
+
 class DecisionTreeNode(object):
     # Constructor
     def __init__(self, att, thr, left, right):  
@@ -16,15 +17,20 @@ class DecisionTreeNode(object):
         self.right = right   
 
     def print_tree(self,indent=''):
-        if self.left  in [0,1]:
-            print(indent+'       ','class=',self.left)
-        else:
-            self.left.print_tree(indent+'    ')
-        print(indent,'if x['+str(self.attribute)+'] <=',self.threshold)
+        # If prints the right subtree, corresponding to the condition x[attribute] > threshold
+        # above the condition stored in the node 
         if self.right  in [0,1]:
             print(indent+'       ','class=',self.right)
         else:
             self.right.print_tree(indent+'    ')
+            
+        print(indent,'if x['+str(self.attribute)+'] <=',self.threshold)
+        
+        if self.left  in [0,1]:
+            print(indent+'       ','class=',self.left)
+        else:
+            self.left.print_tree(indent+'    ')
+        
 
 class DecisionTreeClassifier(object):
     # Constructor
@@ -34,7 +40,7 @@ class DecisionTreeClassifier(object):
         self.min_accuracy = min_accuracy
         
     def fit(self,x,y):  
-        self.root = self._build_tree(x,y,depth=0)
+        self.root = self._id3(x,y,depth=0)
         
     def predict(self,x_test):
         pred = np.zeros(len(x_test),dtype=int)
@@ -42,9 +48,10 @@ class DecisionTreeClassifier(object):
             pred[i] = self._classify(self.root,x_test[i])
         return pred
         
-    def _build_tree(self,x,y,depth):
+    def _id3(self,x,y,depth):
+        orig_entropy = self._entropy(y, [])
         mean_val = np.mean(y)
-        if depth >= self.max_depth or len(y) < self.min_samples_split or max([mean_val,1-mean_val])>=self.min_accuracy:
+        if depth >= self.max_depth or len(y) <= self.min_samples_split or max([mean_val,1-mean_val])>=self.min_accuracy:
             return int(round(mean_val))
         thr = np.mean(x,axis=0)
         entropy_attribute = np.zeros(len(thr))
@@ -52,12 +59,13 @@ class DecisionTreeClassifier(object):
             less = x[:,i] <= thr[i]
             more = ~ less
             entropy_attribute[i] = self._entropy(y[less], y[more])
-        #print(entropy_attribute)
-        best_att = np.argmin(entropy_attribute)  
+        gain = orig_entropy - entropy_attribute
+        # print('Gain:',gain)
+        best_att = np.argmax(gain)  
         less = x[:,best_att] <= thr[best_att]
         more = ~ less
         return DecisionTreeNode(best_att, thr[best_att], int(round(np.mean(y[less]))), int(round(np.mean(y[more]))))
-    
+      
     def _entropy(self,l,m):
         ent = 0
         for p in [l,m]:
@@ -72,7 +80,7 @@ class DecisionTreeClassifier(object):
     def _classify(self, dt_node, x):
         if dt_node in [0,1]:
             return dt_node
-        if x[dt_node.attribute] < dt_node.threshold:
+        if x[dt_node.attribute] <= dt_node.threshold:
             return self._classify(dt_node.left, x)
         else:
             return self._classify(dt_node.right, x)
@@ -80,7 +88,8 @@ class DecisionTreeClassifier(object):
     def display(self):
         print('Model:')
         self.root.print_tree()
-    
+
+
 x = []
 y = []
 infile = open("magic04.txt","r")
@@ -89,10 +98,15 @@ for line in infile:
     x.append(np.fromstring(line[:-2], dtype=float,sep=','))
 infile.close()
     
+xa = np.zeros((len(x),len(x[0])))
+for i in range(len(xa)):
+    xa[i] = x[i]
+x =xa
+
 x = np.array(x).astype(np.float32)
 y = np.array(y) 
 
-#Split data into training and testing
+# Split data into training and testing
 ind = np.random.permutation(len(y))
 split_ind = int(len(y)*0.8)
 x_train = x[ind[:split_ind]]
@@ -107,7 +121,6 @@ elapsed_time = time.time()-start
 print('Elapsed_time training  {0:.6f} '.format(elapsed_time))  
 
 train_pred = model.predict(x_train)
-
 start = time.time()
 test_pred = model.predict(x_test)
 elapsed_time = time.time()-start
@@ -115,7 +128,6 @@ print('Elapsed_time testing  {0:.6f} '.format(elapsed_time))
 
 train_acc = np.sum(train_pred==y_train)/len(train_pred)
 print('train accuracy:', train_acc)
-
 test_acc = np.sum(test_pred==y_test)/len(test_pred)
 print('test accuracy:', test_acc)
 
